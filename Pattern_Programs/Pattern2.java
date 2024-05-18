aafc76e113a02948b8764522bfc83bdba4cf0e62
PriyankaPawar13 <priyapawar13021@gmail.com>
2024-05-18 21:52:07 +0530

    pattern program added

---
+++ Pattern_Programs/Pattern2.java
@@ -0,0 +1,24 @@
+package DAY1;
+
+import java.util.Scanner;
+
+public class Pattern2 {
+	
+	//Triangle Pattern 1
+	
+	public static void main(String args[])
+	{
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter Size : ");
+		int size = scan.nextInt();
+		for(int i=0;i<size;i++)
+		{
+			for(int j=0;j<i;j++)
+			{
+				System.out.print("*");
+			}
+			System.out.println(" ");
+		}
+	}
+
+}
